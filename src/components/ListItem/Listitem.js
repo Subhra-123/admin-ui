57168b386708c3c9e88bbaa644e432e20eea6b0e
Subhrajyoti Naskar <subhrajyotinaskar98@gmail.com>
2022-05-01 08:45:05 +0530

    ListItem component updated

--- src/components/ListItem/Listitem.js
+++ src/components/ListItem/Listitem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Grid from "@mui/material/Grid";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
